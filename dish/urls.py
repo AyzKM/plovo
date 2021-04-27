fa3a2e1bdf060398df8c7e0083356910c4c38e30
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-04-27 19:18:58 +0200

    added dish update and created API

--- dish/urls.py
+++ dish/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from dish.views import DishListAPIView
+from dish.views import DishListAPIView, DishCreateAPIView, \
+    DishUpdateAPIView
 
 urlpatterns = [
     path('', DishListAPIView.as_view(), name='dishes'),
+    path('create/', DishCreateAPIView.as_view(), name='dish-create'),
+#    path('<int:pk>/', DishDetailAPIVIew.as_view(), name='dish'),
+#    path('<int:pk>/delete', DishDeleteAPIVIew.as_view(), name='dish-delete'),
+     path('<int:pk>/update', DishUpdateAPIView.as_view(), name='dish-update'),   
 ]
--- dish/views.py
+++ dish/views.py
@@ -1,12 +1,44 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
 from .models import Dish
-from .serializers import DishListSerializer
+from .serializers import DishListSerializer, DishCreateSerializer, \
+    DishSerializer
 
 class DishListAPIView(APIView):
+    # queryset = Dish.objects.order_by('name')
+    # serializer_class = DishListSerializer()
+
     def get(self, request, *args, **kwargs):
         dishes = Dish.objects.order_by('name')
         dishes_serialized = DishListSerializer(dishes, many=True)
         return Response(data=dishes_serialized.data)
 
+  
+class DishCreateAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        data = request.post
+        serializer = DishCreateSerializer(data=data)
+        if serializer.is_valid():
+            dish_object = serializer.save()
+            return Response(data={'message': 'блюдо добавлено успешно'})
+        return Response(data=serializer.error)
+
+class DishUpdateAPIView(APIView):
+    def put(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        dish = Dish.objects.get(pk=pk)
+
+        data = request.POST
+        serializer = DishUpdateSerializer(data=data) 
+
+        if serializer.is_valid():
+            dish.name = serializer.validated_data.get('name')
+            dish.price = serializer.validated_data.get('price')
+            dish.save()
+            serialized_object = DishSerializer(dish)
+            json_data = serialized_object.data
+            return Response(data=json_data)
+        return Response(data=serializer.error)
+ 
