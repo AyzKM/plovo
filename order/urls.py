e06b2f14d022f3f3f623a78b9dd52f668fd1a172
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-04-29 14:38:38 +0200

    added order list and create

--- order/urls.py
+++ order/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import OrderListCreateView, OrderView
 
-urlpatterns = []
+urlpatterns = [
+    path('', OrderListCreateView.as_view(), name='order-list-create'),
+    path('<int:pk>', OrderView.as_view(), name='order'),
+]
