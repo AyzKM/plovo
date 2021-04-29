b5dc54d49ce002a9280aea68487cc650c5c6fbb8
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-04-29 14:10:14 +0200

    added order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from dish.models import Dish
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        to=User, 
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+
+    dish = models.ForeignKey(
+        to=Dish, 
+        on_delete=models.CASCADE
+    )
+
+    location = models.CharField(max_length=500)
+    phone = models.CharField(max_length=20)
+    discount = models.BooleanField(default=False)
+    discount_sum = models.IntegerField(default=0)
+    status = models.IntegerField(default=0, choices=(
+        (0, 'Accepted'),
+        (1, 'Completed'),
+        (2, 'Canceled'),
+    ))
+
+    def __str__(self):
+        return self.phone
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
