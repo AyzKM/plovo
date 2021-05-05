434e448713fefd1a60467b115410edc5fa791f74
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-05-05 20:29:39 +0200

    added core app/changed admin order/added reqirements

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Order
 
-# Register your models here.
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'dish', 'location', 'status')
+    list_editable = ("status",)
+    search_fields = ('dish__name', 'location')
+    list_filter = ('status',)
+
+admin.site.register(Order, OrderAdmin)
