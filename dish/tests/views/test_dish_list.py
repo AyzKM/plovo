fdbbd142bd618f3f382caeb9f295aa7b9b4d9680
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-05-07 13:56:32 +0200

    modified test dish list

--- dish/tests/views/test_dish_list.py
+++ dish/tests/views/test_dish_list.py
@@ -3,21 +3,40 @@ from rest_framework.test import APITestCase
 from dish.tests.factories import DishFactory
 
 class DishListAPITestCase(APITestCase):
-    def test_get_list_success(self):
-        dishes = []
-        for n in range(3):
-            dishes.append(DishFactory())
+    def setUp(self):
+        self.url = reverse('dishes')
 
-        url = reverse('dishes')
-        response = self.client.get(url)
+    def test_get_list_success(self):
+        dish_1 = DishFactory(name='Лагман', price=130)
+        dish_2 = DishFactory()
+
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.data, list)
-        expected_data = {
-            'id': 2,
-            'name': 'Dish number 1',
-            'price': 120
+
+        expected_data_1 = {
+            'id': dish_1.id,
+            'name': 'Лагман',
+            'price': 130
         }
-        self.assertEqual(expected_data, response.data[1])
+
+        expected_data_2 = {
+            'id': dish_2.id,
+            'name': dish_2.name,
+            'price': dish_2.price
+        }
+        self.assertEqual(expected_data_1, response.data[1])
+        self.assertEqual(expected_data_2, response.data[0])
+
+    def test_post_dish_list_should_return_405(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 405)
 
 
 
